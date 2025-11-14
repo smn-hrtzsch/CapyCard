83650e2429bd0a009c7710063b9a26a0bf0ecb3b
smn-hrtzsch <simon@hoertzsch.de>
2025-11-14 17:04:31 +0100

    refactor: Minor whitespace/newline adjustments in Program.cs

--- FlashcardApp/Application/Program.cs
+++ FlashcardApp/Application/Program.cs
@@ -47,15 +47,15 @@ namespace FlashcardApp
 
                     db.Cards.AddRange(
                         new Card { DeckId = deck.Id, Front = "Meine liebste Erinnerung", Back = "Gemeinsam den Sonnenuntergang an unserem \"Privatstrand\" in Kroatien beobachten." },
-                        new Card { DeckId = deck.Id, Front = "Was ich so an dir schätze", Back = "Deine Zielstrebigkeit \n Deine Kraft für den Alltag \n Deinen Fleiß \n Deine Liebe zur Bibel \n Deine Ehrlichkeit \n Deine Kreativität \n Deinen Mut" },
-                        new Card { DeckId = deck.Id, Front = "Was ich so an dir liebe", Back = "Deine Empathie \n Deine Fürsorglichkeit \n Deine Fürsorge \n Deine Gutherzigkeit \n Deine Witze und Humor \n Deine süße Art \n Dein Lächeln" },
-                        new Card { DeckId = deck.Id, Front = "Meine Lieblingsmomente", Back = "Erstes Date im Kino in Freiberg und das Kuscheln danach auf der Parkbank \n Als ich Talisma kennenlernen durfte \n Die vielen Gottesdienstbesuche \n Wenn wir zusammen im Bett kuscheln und Serien schauen \n Wenn wir uns ausgiebig umarmem" },
-                        new Card { DeckId = deck.Id, Front = "Was ich an deinem Aussehen liebe", Back = "Deine wunderschönen Augen \n Dein zauberhaftes Lächeln \n Deine Haare \n Dein immer hübscher Kleidungsstil \n Alles andere an deinem Körper" },
+                        new Card { DeckId = deck.Id, Front = "Was ich so an dir schätze", Back = "Deine Zielstrebigkeit \nDeine Kraft für den Alltag \nDeinen Fleiß \nDeine Liebe zur Bibel \nDeine Ehrlichkeit \nDeine Kreativität \nDeinen Mut" },
+                        new Card { DeckId = deck.Id, Front = "Was ich so an dir liebe", Back = "Deine Empathie \nDeine Fürsorglichkeit \nDeine Fürsorge \nDeine Gutherzigkeit \nDeine Witze und Humor \nDeine süße Art \nDein Lächeln" },
+                        new Card { DeckId = deck.Id, Front = "Meine Lieblingsmomente", Back = "Erstes Date im Kino in Freiberg und das Kuscheln danach auf der Parkbank \nAls ich Talisma kennenlernen durfte \nDie vielen Gottesdienstbesuche \nWenn wir zusammen im Bett kuscheln und Serien schauen \nWenn wir uns ausgiebig umarmen" },
+                        new Card { DeckId = deck.Id, Front = "Was ich an deinem Aussehen liebe", Back = "Deine wunderschönen Augen \nDein zauberhaftes Lächeln \nDeine Haare \nDein immer hübscher Kleidungsstil \nAlles andere an deinem Körper" },
                         new Card { DeckId = deck.Id, Front = "Was mich glücklich macht", Back = "Dass wir immer zusammenhalten, egal was ist. Dafür liebe ich dich und unsere Beziehung." },
                         new Card { DeckId = deck.Id, Front = "Was ich mir wünsche", Back = "Dass wir für immer zusammenbleiben" },
                         new Card { DeckId = deck.Id, Front = "Wofür ich Gott ganz besonders dankbar bin", Back = "Dass er dich geschaffen hat und mir an meine Seite gegeben hat" },
                         new Card { DeckId = deck.Id, Front = "Was du niemals vergessen darfst", Back = "Ich LIEBE dich über alles und das wird auch immer so bleiben!" },
-                        new Card { DeckId = deck.Id, Front = "Meine Schwachstelle", Back = "Du und Talisma, weil ich ohne euch niemals mehr leben wollen würde. \n Und Natürlich Nutella pah." }
+                        new Card { DeckId = deck.Id, Front = "Meine Schwachstelle", Back = "Du und Talisma, weil ich ohne euch niemals mehr leben wollen würde. \nUnd Natürlich Nutella pah." }
                     );
                     db.SaveChanges();
                 }
