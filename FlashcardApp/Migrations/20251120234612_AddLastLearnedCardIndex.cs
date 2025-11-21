c4d2d86d7f3b8d121b107bb9b4e9621d181454d6
smn-hrtzsch <simon@hoertzsch.de>
2025-11-21 08:16:29 +0100

    Fix empty migration AddLastLearnedCardIndex

--- FlashcardApp/Migrations/20251120234612_AddLastLearnedCardIndex.cs
+++ FlashcardApp/Migrations/20251120234612_AddLastLearnedCardIndex.cs
@@ -10,13 +10,20 @@ namespace FlashcardApp.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<int>(
+                name: "LastLearnedCardIndex",
+                table: "Decks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "LastLearnedCardIndex",
+                table: "Decks");
         }
     }
 }
